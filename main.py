79e86ca5b7a3b84eb8f0f65dfef61e41d6be7f39
„Andi <ajerni@gmail.com>
2023-09-08 15:58:30 +0200

    ca marche

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ast
 
 
 if __name__ == "__main__":
-    q = "A simple delay"  # example "A short delay" works
+    q = "A light distortion running into a big reverb"  # example "A short delay" works
     sel_dict_string = generateEffect(q)
 
     sel_dict_dict = ast.literal_eval(sel_dict_string)
